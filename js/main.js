0be94342470d0c0d15213837e801d938f089e114
Renato Perez <renato.sanlorenzo@gmail.com>
2022-07-27 17:39:06 -0300

    segunda entrega proyecto final

--- js/main.js
+++ js/main.js
@@ -1,82 +1,78 @@
-class Producto {
-    constructor(id, nombre, precio, categoria) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.categoria = categoria;
-    }
-    mostrarProducto() {
-        return (
-            "id: " +
-            this.id +
-            " Precio: " +
-            this.precio +
-            " " +
-            " Producto: " +
-            this.nombre +
-            "\n"
-        );
-    }
-}
-
-function comprar(nombre, email, tel, productosEnCarro) {
-    let cant = productosEnCarro.reduce((acc, item) => item.precio + acc, 0);
-    alert("Gracias" + nombre + " por tu compra. \n Total: $" + cant);
-}
-
 let productos = [
-    new Producto(1001, 'zapatillas adidas', 500, 'elegante'),
-    new Producto(1002, 'zapatillas nike', 500, 'urbano'),
-    new Producto(1003, 'zapatillas lacoste', 500, 'elegante'),
-    new Producto(1004, 'zapatillas adidas', 500, 'urbano'),
-
-    new Producto(2001, 'conjunto drew', 1000, 'urbano'),
-    new Producto(2002, 'campera nautica negra', 500, 'deportivo'),
-    new Producto(2003, 'campera nautica azul', 500, 'deportivo'),
-    new Producto(2004, 'buzo drew', 500, 'urbano'),
-
+    {id: 102, nombre: "Zapatilla nike", precio:20000 },
+    {id: 103, nombre: "Zapatilla nike", precio: 20000},
+    {id: 104, nombre: "buzo drew", precio: 10000},
+    {id: 105, nombre: "Zapatillas lacoste", precio:25500 },
+    {id: 106, nombre: "Zapatilla nike", precio: 25500},
+    {id: 107, nombre: "Zapatilla nike", precio: 20000},
+    {id: 108, nombre: "zapatillas lacoste", precio: 25500},
+    {id: 109, nombre: "zapatillas lacoste", precio: 20500},
+    {id: 110, nombre: "campera nautica azul", precio: 30000},
 ];
 
-let categorias = ["urbano, deportivo,elegante"];
+let aux = localStorage.getItem("productosEnCarro");
 
-let productosEnCarro = [];
+let productosEnCarro;
 
-let categoria = "";
-
-
-while (categoria != "salir" && categoria != null) {
-    let aux = categorias.join("\n");
-    categoria = prompt(
-        'Ingrese un categoria para continuar con su compra o ingrese "salir": \n(' + aux + ")"
-    );
-
-    if (categoria != "salir" && categoria != null) {
-        let productosFiltradoPorCategoria = productos.filter(
-            (item) => item.categoria == categoria
-        );
-
-
-        let cartel = "";
-        for (let i = 0; i < productosFiltradoPorCategoria.length; i++) {
-            cartel += productosFiltradoPorCategoria[i].mostrarProducto();
-        }
-
-        let idSeleccionado = parseInt(
-            prompt("Seleccione el id del producto que quiere comprar: \n\n" + cartel)
-        );
-
-        let productoParaCarro = productosFiltradoPorCategoria.find(
-            (item) => item.id == idSeleccionado
-        );
-
-        if (productoParaCarro) {
-            productosEnCarro.push(productoParaCarro);
-        }
-    }
+if (!aux) {
+    productosEnCarro = [];
+}else {
+    productosEnCarro = JSON.parse(aux);
+    listadoProductosEnCarro();
 }
 
+
+
+function listadoProductos() {
+    let aux = '';
+    for (let i = 0; i < productos.length; i++) {
+        aux = aux +  `<div onclick= "meterAlCarro({id: ${productos[i].id}, nombre: '${productos[i].nombre}', precio: ${productos[i].precio}})" style="cursor: pointer;border: 1px solid black; ">
+                    <h3>${productos[i].nombre}</h3>
+                    <p>${productos[i].precio}</p>
+                    <p> id: ${productos[i].id}</p>
+                    </div> `;
+    }
+    document.getElementById('div-productos').innerHTML = aux;
+}
+
+
+
+listadoProductos();
+
+//-----CARRO-----//
+
+function meterAlCarro (objetoProducto){
+    productosEnCarro.push(objetoProducto);
+
+    localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+    listadoProductosEnCarro();
+}
+
+function BorrarDelCarro (id) {
+    productosEnCarro.splice(id, 1);
+
+    localStorage.setItem("productosEnCarro", JSON.stringify(productosEnCarro));
+    listadoProductosEnCarro();
+}
+
+
+function listadoProductosEnCarro() {
+    let aux = '';
+    for (let i = 0; i < productosEnCarro.length; i++) {
+        aux = aux + `<div style ="border: 1px solid green;">
+                        <h3>${productosEnCarro[i].nombre}</h3>
+                        <p>${productosEnCarro[i].precio}</p>
+                        <p> id: ${productosEnCarro[i].id}</p>
+                        <p onclick= "BorrarDelCarro(${i})" style = "cursor: pointer;"><button>BORRAR DEL CARRO</button></p>
+                    </div> `;
+    }
+    document.getElementById('div-carrito').innerHTML = aux;
+}
+
+//-----TERMINA CARRO---///
+
 if (productosEnCarro.length > 0) {
-    alert('Te invitamos a Iniciar sesion o Registrarte para terminar tu compra');
+    alert('Te invitamos a Iniciar sesion o Registrarte para iniciar su compra');
     let nombre = prompt('ingrese su nombre');
     let mail = prompt('ingrese su email');
     let tel = prompt('ingrese su tel');
